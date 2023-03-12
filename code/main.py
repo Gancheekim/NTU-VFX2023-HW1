04d5cfd317ac2e5d458982bc8c93228e4129f20b
Gancheekim <b07901133@ntu.edu.tw>
2023-03-12 21:13:29 +0800

    finish preprocesses and MTB. future work: debevic HDR algo

--- code/main.py
+++ code/main.py
@@ -2,8 +2,7 @@ import numpy as np
 from PIL import Image
 import matplotlib.pyplot as plt
 from argparse import ArgumentParser
-import utils
-
+from mtb import MTB
 
 def getDatasetImageName(dataset):
     if dataset == "memorial":
@@ -14,7 +13,7 @@ def getDatasetImageName(dataset):
     if dataset == "debug":
         prefix_name = "./../data/debug/p"
         sufix_name = ".jpg"
-        imgs_name = [prefix_name+str(idx)+sufix_name for idx in range(1,3)]
+        imgs_name = [prefix_name+str(idx)+sufix_name for idx in range(1,5)]
     
     return imgs_name
 
@@ -22,30 +21,18 @@ def main(args):
     imgs_name = getDatasetImageName(args.dataset)
     imgs = [np.asarray(Image.open(img_name).convert('RGB')) for img_name in imgs_name] # read image as numpy array
 
-    img1 = imgs[0]
-    img2 = imgs[1]
-    mtb = utils.MTB(5)
+    mtb = MTB(8)
+    shift_imgs = mtb.run(imgs, True) # list of numpy array, which are the aligned images
 
-    offsets = mtb.mtb(img2, img1)
-    img3 = mtb.shift(img2, offsets[1], offsets[0])
+    # TODO: finish HDR algorithm
 
-    
-    plt.subplot(1,3,1)
-    plt.imshow(img2)
-    plt.title('fix image')
-    plt.subplot(1,3,2)
-    plt.imshow(img1)
-    plt.title('image to be aligned')
-    plt.subplot(1,3,3)
-    plt.imshow(img3)
-    plt.title('alignment result')
-    plt.show()
+        
     return
 
 if __name__ == '__main__':
     parser = ArgumentParser()
     '''add argument here'''
-    parser.add_argument("--dataset", type=str, default="memorial", choices=["memorial", "debug"]) # currently debug mode is for testing MTB
+    parser.add_argument("--dataset", type=str, default="debug", choices=["memorial", "debug"]) # currently debug mode is for testing MTB
     args = parser.parse_args()
     main(args)
 
