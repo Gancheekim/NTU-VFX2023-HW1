ac2c3cb3291f1adfebcf98481bdcde747885b87b
David Akas <r11942163@ntu.edu.tw>
2023-03-25 10:39:02 +0800

    make code more concise

--- code/hdr_generator.py
+++ code/hdr_generator.py
@@ -43,69 +43,92 @@ def get_exposure(txt_path):
         # print(f"Exposure time list: {exposure_time_list}")
         return exposure_time_list
 
-def uniform_img_sampler(imgs_list, P, H, W, N, channel_num=0):
+def uniform_img_sampler(imgs_list, P, H, W, N, num_channels=0):
     ''' Samples image at specific channel
     '''
-    sample_row_num = np.linspace(0, H-1, num=N, dtype=int)    # which rows to sample from the original image (i)
-    sample_col_num = np.linspace(0, W-1, num=N, dtype=int)   # which columns to sample from the original image (j)
-    Z_sampled = np.zeros((P, N, N))   
+    sample_row_num = np.linspace(0, H-1, num=N, dtype=int)   # which rows to sample from the original image (i)
+    sample_col_num = np.linspace(0, W-1, num=N, dtype=int)   # which columns to sample from the original image (j) 
+    Z_samples = []
 
-    for img_idx, img in enumerate(imgs_list):
-        for Z_i, i in enumerate(sample_row_num):
-            for Z_j, j in enumerate(sample_col_num):
-                Z_sampled[img_idx][Z_i][Z_j] = img[i][j][channel_num]    
+    # Sampling
+    for channel_n in range(num_channels):
+        Z_sampled = np.zeros((P, N, N))
+        for img_idx, img in enumerate(imgs_list):
+            for Z_i, i in enumerate(sample_row_num):
+                for Z_j, j in enumerate(sample_col_num):
+                    Z_sampled[img_idx][Z_i][Z_j] = img[i][j][channel_n] 
+        Z_samples.append(Z_sampled)
 
     #print(f"Sample x-coords: {sample_col_num}\nSample y-coords: {sample_row_num}")
-    return Z_sampled
+    return Z_samples
 
-def get_response_func(Z, B, Lambda, w):
+def get_response_func(Z_samples, B, Lambda, w):
     '''
+    Solve Ax=b linear problem. Reference: MatLab code from lec03.pdf of lecture.
     Z: the pixel values of pixel location number i in image p; shape = P x N*N
     B: the log base 2 of exposure time
     Lambda: Lambda
     w: weighting
     '''
-    #Z = Z.flatten()  # flatten the sample array, this will have size P*N*N
     n = 256
-    Z = Z.reshape(Z.shape[0], Z.shape[1] * Z.shape[2]) # shape: P x N*N
-    Z_num_rows = np.size(Z, 0)
-    Z_num_cols = np.size(Z, 1)
-    A = np.zeros((Z_num_rows * Z_num_cols + n + 1, n + Z_num_cols), dtype=np.float32)
-    
-    A_num_rows = np.size(A, 0)
-    b = np.zeros((A_num_rows, 1), dtype=np.float32)
+    g_funcs = []
+    lsq_errors = []
+    for Z in Z_samples:
+        # Make matrix A
+        Z = Z.reshape(Z.shape[0], Z.shape[1] * Z.shape[2]) # shape: P x N*N
+        Z_num_rows = np.size(Z, 0)
+        Z_num_cols = np.size(Z, 1)
+        A = np.zeros((Z_num_rows * Z_num_cols + n + 1, n + Z_num_cols), dtype=np.float32)
+        
+        # Make matric b
+        A_num_rows = np.size(A, 0)
+        b = np.zeros((A_num_rows, 1), dtype=np.float32)
 
-    
-    # Include the data-fitting equations
-    k = 0
-    for i in range(Z_num_rows):
-        for j in range(Z_num_cols):
-            z = int(Z[i][j])
-            wij = w[z]
-            A[k][z] = wij
-            A[k][n+j] = -wij
-            b[k] = wij * B[i]
-            k += 1
-    # Fix the curve by setting the middle value to 0
-    A[k][128] = 1
-    k += 1
-
-    # Include the smoothness equations
-    for i in range(n-1):
-        A[k][i] = Lambda * w[i+1]
-        A[k][i+1] = -2 * Lambda * w[i+1]
-        A[k][i+2] = Lambda * w[i+1]
+        
+        # Include the data-fitting equations
+        k = 0
+        for i in range(Z_num_rows):
+            for j in range(Z_num_cols):
+                z = int(Z[i][j])
+                wij = w[z]
+                A[k][z] = wij
+                A[k][n+j] = -wij
+                b[k] = wij * B[i]
+                k += 1
+        # Fix the curve by setting the middle value to 0
+        A[k][128] = 1
         k += 1
-    # Solve the system using SVD
-    x = np.linalg.lstsq(A, b, rcond=None)[0]
-    g = x[:n]
-    lE = x[n:]
 
-    return g, lE
+        # Include the smoothness equations
+        for i in range(n-1):
+            A[k][i] = Lambda * w[i+1]
+            A[k][i+1] = -2 * Lambda * w[i+1]
+            A[k][i+2] = Lambda * w[i+1]
+            k += 1
+        # Solve the system using SVD
+        x = np.linalg.lstsq(A, b, rcond=None)[0]
+        g = x[:n]
+        lE = x[n:]
+
+        # Record the results
+        g_funcs.append(g)
+        lsq_errors.append(lE)
+
+    return g_funcs, lsq_errors
 
 
 
-def hdr_gen_debevec(imgs_list, exp_time_list, P, N=20):
+def plot_response_curve(g_funcs):
+    plt.figure(figsize=(10,10))
+    g_func_r, g_func_g, g_func_b = g_funcs
+    plt.plot(g_func_r, range(256), 'red')
+    plt.plot(g_func_g, range(256), 'green')
+    plt.plot(g_func_b, range(256), 'blue')
+    plt.ylabel("Pixel Value Z")
+    plt.xlabel("Log Exposure x")
+    plt.savefig("testcurve.png")
+
+def get_hdr_debevec(imgs_list, exp_time_list, P, N=20):
     '''
     1. Sample from each channel of each images
     2. Find function g() of each channel
@@ -132,29 +155,15 @@ def hdr_gen_debevec(imgs_list, exp_time_list, P, N=20):
             weight.append(Zmax - z)
     
     # Sample pixel points from images
-    # Todo: Check if PIL is in fact read as RGB
-    Z_sampled_R = uniform_img_sampler(imgs_list, P, H, W, N, channel_num=0)  # shape: P x N x N
-    Z_sampled_G = uniform_img_sampler(imgs_list, P, H, W, N, channel_num=1)
-    Z_sampled_B = uniform_img_sampler(imgs_list, P, H, W, N, channel_num=2)
-
+    Z_samples = uniform_img_sampler(imgs_list, P, H, W, N, num_channels=3)  # list of length num_channels, of items shape: P x N x N
 
     # Solve Ax=b (try single channel)
-    g_R, lE_R = get_response_func(Z_sampled_R, B, Lambda, weight)
-    g_G, lE_G = get_response_func(Z_sampled_G, B, Lambda, weight)
-    g_B, lE_B = get_response_func(Z_sampled_B, B, Lambda, weight)
-    
-    # Plot g Curves
-    plt.figure(figsize=(10,10))
-    plt.plot(g_R, range(256), 'rx')
-    plt.plot(g_G, range(256), 'gx')
-    plt.plot(g_B, range(256), 'bx')
-    plt.ylabel("Pixel Value Z")
-    plt.xlabel("Log Exposure x")
-    plt.savefig("testcurve.png")
+    g_funcs, lsq_errors = get_response_func(Z_samples, B, Lambda, weight)
 
+    # Plot g Curves
+    plot_response_curve(g_funcs)
 
     return 0
-
 
 
 
@@ -172,7 +181,7 @@ def main(args):
     N    = 20                                # number of sample point pixels per image
     exps = get_exposure(memorial_txt_path)   # list of exposure times for all images
     P    = len(exps)                         # number of images
-    hdr_images = hdr_gen_debevec(imgs, exps, P, N)
+    hdr_images = get_hdr_debevec(imgs, exps, P, N)
 
         
     return
